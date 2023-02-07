907834135f6c39cd20e187ad2579ca24b27f71ec
Emerson Viana <missonzito@outlook.com>
2023-02-07 20:26:41 -0300

    useStates, hook do React, usuario e senha

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,19 +1,43 @@
+import { useState } from 'react'
+
 function Form() {
 
     function cadastrarUsuario(e) {
-        console.log('Cadastrou o usuario !') 
+       console.log(`Usuario ${name} foi Cadastrado com a Senha ${password}`)
         e.preventDefault()       
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return (
         <div>
             <h1>Meu Cadastro</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Nome Completo" />
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                    type="text" 
+                    id="name" 
+                    name="name" 
+                    placeholder="Nome Completo" 
+                    onChange={(e) => setName(e.target.value)}
+                    />
                 </div>
                 <div>
-                    <input type="submit" value="Cadastrar" />
+                    <label htmlFor="password">Senha:</label>
+                    <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="Digite a Sua Senha" 
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input 
+                    type="submit" 
+                    value="Cadastrar" />
                 </div>
             </form>
         </div>
